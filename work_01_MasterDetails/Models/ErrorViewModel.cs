123bffbe7b3221f6a2359e47e4112e5cab1de264
asnaee <asnaeem38@gmail.com>
2023-09-08 21:53:00 +0600

    Add project files.

---
+++ work_01_MasterDetails/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace work_01_MasterDetails.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
